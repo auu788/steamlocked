fef71d928f9d5f9860427dfedbb6710ae7939708
auu788 <auu788@gmail.com>
2018-10-01 22:58:20 +0200

    Dockerized backend api

--- backend/api/src/db.js
+++ backend/api/src/db.js
@@ -1,10 +1,10 @@
 import mysql from 'mysql';
 
 let connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'steamlocked'
+    host: 'mariadb',
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DATABASE
 });
 
 connection.connect((err) => {
